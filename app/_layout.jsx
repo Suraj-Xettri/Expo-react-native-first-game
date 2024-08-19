8c0266963d374cf527562d3c857dddbb1b134365
Suraj Thapa <surajthapart@gmail.com>
2024-08-19 22:14:55 +0545

    wrote my first react native name and displayed

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,9 +1,12 @@
-import { Stack } from "expo-router";
-
-export default function RootLayout() {
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Slot, Stack } from "expo-router";
+const RootLayout = () => {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="index" options={{headerShown: false}}/>
     </Stack>
   );
-}
+};
+
+export default RootLayout;
--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text>Sora.</Text>
+
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{color:'blue'}}>Go to profile</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
